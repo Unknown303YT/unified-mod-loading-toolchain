9e7fe7b48bb1668bedd9b28980580d33abb8a4df
Unknown_303YT <unknown.303yt@gmail.com>
2025-12-02 17:28:11 +1030

    Begin work on Mappings.
    Signed-off-by: Unknown_303YT <unknown.303yt@gmail.com>

--- src/main/java/com/riverstone/unknown303/umlt/util/TinyMappingUtil.java
+++ src/main/java/com/riverstone/unknown303/umlt/util/TinyMappingUtil.java
@@ -1,5 +1,14 @@
 package com.riverstone.unknown303.umlt.util;
 
+import net.fabricmc.mappingio.MappingReader;
+import net.fabricmc.mappingio.format.MappingFormat;
+
+import java.io.File;
+
 public final class TinyMappingUtil {
     private TinyMappingUtil() {}
+
+    public static void officialToTiny(File officialMapping, File outputFile) {
+        MappingReader.read(officialMapping.toPath(), MappingFormat.); // TODO: Investigate https://github.com/FabricMC/fabric-loom/tree/dev/1.13/src/main/java/net/fabricmc/loom/configuration/providers/mappings/mojmap
+    }
 }
